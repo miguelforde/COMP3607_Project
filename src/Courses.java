383b24124cff64deb87b36ace746fd7624c90a94
miguelforde <62704578+miguelforde@users.noreply.github.com>
2020-12-02 04:26:58 -0400

    Update Courses.java

--- src/Courses.java
+++ src/Courses.java
@@ -4,20 +4,29 @@ public class Courses implements CourseListing {
     private String courseCode;  
     private String title;
     private int credits; 
-    private Courses prerequisite;  
+    private Courses prerequisite1, prerequisite2;  
     private double qualityGrade;
 
-    public Courses(String courseCode,int credits,Courses prerequisite,String title){ 
+    public Courses(String courseCode,int credits,Courses prerequisite1,String title){ 
         this.courseCode = courseCode; 
         this.credits = credits; 
-        this.prerequisite = prerequisite; 
+        this.prerequisite1 = prerequisite1; 
         this.title = title;
-    }//end Courses()  
-
-    public Courses(String courseCode,int credits,Courses prerequisite,double qualityGrade,String title){ 
+    }//end Courses()
+    
+    public Courses(String courseCode, int credits, Courses prerequisite1, Courses prerquisite2, double qualityGrade,String title){ 
         this.courseCode = courseCode; 
         this.credits = credits; 
-        this.prerequisite = prerequisite;  
+        this.prerequisite1 = prerequisite1;
+        this.prerequisite2 = prerequisite2;
+        this.title = title;
+        this.qualityGrade = qualityGrade;
+    }//end Courses()
+    
+    public Courses(String courseCode,int credits,Courses prerequisite1,double qualityGrade,String title){ 
+        this.courseCode = courseCode; 
+        this.credits = credits; 
+        this.prerequisite1 = prerequisite1;  
         this.title = title;
         this.qualityGrade = qualityGrade;
     }//end Courses()
